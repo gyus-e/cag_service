f92e6556abff86e6f35c90540fc00e49bc1b0501
gyus-e <giuseppe.amato8@studenti.unina.it>
2025-07-17 15:49:37 +0200

    removed commented code

--- utils/validators.py
+++ utils/validators.py
@@ -36,32 +36,3 @@ def _validate_data(request: Request):
     if not data:
         raise ValueError("Invalid request: No data provided")
     return data
-
-
-# TODO: Move to rag_service
-# def validate_rag_configuration(request_rag_configuration: dict | None) -> dict | None:
-#     if not request_rag_configuration or not isinstance(request_rag_configuration, dict):
-#         print("No rag_configuration provided or it is not a dictionary")
-#         return None
-
-#     embed_model_name = request_rag_configuration.get("embed_model_name")
-
-#     if not embed_model_name or not isinstance(embed_model_name, str):
-#         raise ValueError("embed_model_name must be a non-empty string")
-
-#     request_temperature = request_rag_configuration.get("temperature")
-#     request_top_p = request_rag_configuration.get("top_p")
-#     request_top_k = request_rag_configuration.get("top_k")
-
-#     temperature = float(request_temperature) if request_temperature is not None else 0.0
-#     do_sample = True if abs(temperature) < 1e-8 else False
-#     top_p = float(request_top_p) if request_top_p is not None else 0.9
-#     top_k = int(request_top_k) if request_top_k is not None else 50
-
-#     return {
-#         "embed_model_name": embed_model_name,
-#         "temperature": temperature,
-#         "do_sample": do_sample,
-#         "top_p": top_p,
-#         "top_k": top_k
-#     }
